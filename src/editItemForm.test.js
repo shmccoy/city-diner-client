92ca6e95bb878086c9cefd010e8733f0c06e045f
Sean M <53903852+shmccoy@users.noreply.github.com>
2020-06-29 00:11:41 -0400

    edit

--- src/editItemForm.test.js
+++ src/editItemForm.test.js
@@ -1,16 +1,16 @@
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import EditItemForm from "./editItemForm";
+import React from "react";
+import ReactDOM from "react-dom";
+import EditItemForm from "./editItemForm";
 
-// import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
-// it("renders without crashing", () => {
-//   const div = document.createElement("div");
-//   ReactDOM.render(
-//     <BrowserRouter>
-//       <EditItemForm />
-//     </BrowserRouter>,
-//     div
-//   );
-//   ReactDOM.unmountComponentAtNode(div);
-// });
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(
+    <BrowserRouter>
+      <EditItemForm />
+    </BrowserRouter>,
+    div
+  );
+  ReactDOM.unmountComponentAtNode(div);
+});
